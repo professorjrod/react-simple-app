a0880b1bb56fe275871b5d2e1b034740e7015ba3
Jared <mumawjared@gmail.com>
2022-07-21 17:08:37 -0700

    data

---
+++ data/data.js
@@ -0,0 +1,170 @@
+const jokes = [
+      {
+        category: "Misc",
+        type: "single",
+        joke: "Relationship Status: just tried to reach for my dog's paw and he pulled it away so I pretended I was reaching for the remote.",
+        flags: {
+          nsfw: false,
+          religious: false,
+          racist: false,
+          sexist: false,
+          political: false,
+          explicit: false
+        },
+        id: 195,
+        safe: true,
+        lang: "en"
+      },
+      {
+        category: "Programming",
+        type: "single",
+        joke: "If Bill Gates had a dime for every time Windows crashed ... Oh wait, he does.",
+        flags: {
+          nsfw: false,
+          religious: false,
+          political: false,
+          racist: false,
+          sexist: false,
+          explicit: false
+        },
+        id: 22,
+        safe: true,
+        lang: "en"
+      },
+      {
+        category: "Pun",
+        type: "twopart",
+        setup: "Why did the koala get rejected?",
+        delivery: "Because he did not have any koalafication.",
+        flags: {
+          nsfw: false,
+          religious: false,
+          political: false,
+          racist: false,
+          sexist: false,
+          explicit: false
+        },
+        id: 192,
+        safe: true,
+        lang: "en"
+      },
+      {
+        category: "Misc",
+        type: "twopart",
+        setup: "What does the mermaid wear to math class?",
+        delivery: "Algae-bra.",
+        flags: {
+          nsfw: false,
+          religious: false,
+          political: false,
+          racist: false,
+          sexist: false,
+          explicit: false
+        },
+        safe: false,
+        id: 272,
+        lang: "en"
+      },
+      {
+        category: "Pun",
+        type: "twopart",
+        setup: "What did the customer say to the waiter?",
+        delivery: "I'm all fed up with your service.",
+        flags: {
+          nsfw: false,
+          religious: false,
+          political: false,
+          racist: false,
+          sexist: false,
+          explicit: false
+        },
+        id: 190,
+        safe: true,
+        lang: "en"
+      },
+      {
+        category: "Dark",
+        type: "twopart",
+        setup: "What did one necrophiliac tell the other necrophiliac after work?",
+        delivery: "\"Let's go have a couple of cold ones!\"",
+        flags: {
+          nsfw: true,
+          religious: false,
+          political: false,
+          racist: false,
+          sexist: false,
+          explicit: true
+        },
+        id: 115,
+        safe: false,
+        lang: "en"
+      },
+      {
+        category: "Misc",
+        type: "single",
+        joke: "A perfectionist walked into a bar... apparently, the bar was not set high enough.",
+        flags: {
+          nsfw: false,
+          religious: false,
+          political: false,
+          racist: false,
+          sexist: false,
+          explicit: false
+        },
+        safe: true,
+        id: 288,
+        lang: "en"
+      },
+      {
+        category: "Pun",
+        type: "twopart",
+        setup: "How did Harry Potter get down the hill?",
+        delivery: "Walking...\nJK, Rolling.",
+        flags: {
+          nsfw: false,
+          religious: false,
+          political: false,
+          racist: false,
+          sexist: false,
+          explicit: false
+        },
+        id: 206,
+        safe: true,
+        lang: "en"
+      },
+      {
+        category: "Pun",
+        type: "twopart",
+        setup: "How much did your chimney cost?",
+        delivery: "Nothing, it was on the house.",
+        flags: {
+          nsfw: false,
+          religious: false,
+          political: false,
+          racist: false,
+          sexist: false,
+          explicit: false
+        },
+        id: 231,
+        safe: true,
+        lang: "en"
+      },
+      {
+        category: "Dark",
+        type: "single",
+        joke: "I'll never forget my Granddad's last words to me just before he died. \"Are you still holding the ladder?\\",
+        flags: {
+          nsfw: false,
+          religious: false,
+          political: false,
+          racist: false,
+          sexist: false,
+          explicit: false
+        },
+        id: 208,
+        safe: false,
+        lang: "en"
+      }
+    
+]
+export default jokes;
